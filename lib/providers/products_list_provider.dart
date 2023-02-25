e359591dc81b51cd47b0f3294ee1f464a2b8ab74
Thiago de Sales <thiagobrunoms@gmail.com>
2023-02-25 09:45:10 -0300

    Remove old providers

--- lib/providers/products_list_provider.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:sora_studies/data/models/products_list.dart';
-// import 'package:sora_studies/domain/products_list_notifier.dart';
-// import 'package:sora_studies/providers/product_provider.dart';
-
-// final productsListProvider = StateNotifierProvider<ProductListNotifier, AsyncValue<ProductsList>>(
-//   (ref) => ProductListNotifier(ref.read(productRepositoryProvider))..loadProducts()
-// );
--- lib/providers/selected_product_provider.dart
+++
@@ -1,5 +0,0 @@
-// final import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:sora_studies/data/models/product.dart';
-// import 'package:sora_studies/domain/selected_product_notifier.dart';
-
-// final selectedProductSNP = StateNotifierProvider<SelectedProductStateNotifier, Product>((ref) =>  )
